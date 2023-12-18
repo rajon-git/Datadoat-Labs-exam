c09e9c4487c98069463a0088009144da800ac0b9
rajon-git <rajon.zhsust15@gmail.com>
2023-12-18 17:21:07 +0600

    course model added

---
+++ models/Course.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CourseSchema = new Schema(
+  {
+    courseName: {
+      type: String,
+      required: true
+    },
+    courseDescription: {
+      type: String,
+      required: true
+    },
+    instructor: { type: Schema.Types.ObjectId, ref: "User" },
+    category: { type: Schema.Types.String, ref: "Category" }
+  },
+  { timestamps: { createdAt: "created_at" } }
+);
+
+const Course=mongoose.model("courses",CourseSchema);
+module.exports=Course;
