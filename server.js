54348ced12a8369398936be17008baf5ebb18375
rajon-git <rajon.zhsust15@gmail.com>
2023-12-18 17:12:00 +0600

    server setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const fileUpload = require('express-fileupload');
+var multer = require('multer')
+var cors = require('cors');
+
+const app = express();
+
+app.use(fileUpload());
+//Body Parser
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit:1000000}));
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+const port = process.env.PORT || 8000;
+
+mongoose.connect(process.env.DATABASE)
+        .then(()=>{
+            app.listen(port,()=>{
+                console.log(`Server is running on port ${port}`);
+            });
+        })
+        .catch((error)=> console.log(error));
