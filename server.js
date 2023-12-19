12c3b4ba6e470fae6a84144314419398b013792b
rajon-git <rajon.zhsust15@gmail.com>
2023-12-19 21:00:50 +0600

    server fixed

--- server.js
+++ server.js
@@ -2,33 +2,44 @@ const { readdirSync } = require("fs");
 const express = require("express");
 const mongoose = require("mongoose");
 const fileUpload = require('express-fileupload');
-const multer = require('multer');
 const morgan = require("morgan");
 const helmet = require("helmet");
 require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
+
+// Middleware
 app.use(helmet());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use(fileUpload());
 app.use(cors());
 
-readdirSync("./routes").map(r => app.use("/api/v1", require(`./routes/${r}`)));
+// Routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require(`./routes/${route}`))
+);
 
+// 404 Route
 app.use('*', (req, res) => {
-  res.status(404).json({status: 'failed', data: 'Hello World'});
+  res.status(404).json({ status: 'failed', data: 'Not Found' });
 });
 
 const port = process.env.PORT || 8000;
 
-
+// Database Connection
 mongoose
-  .connect(process.env.DATABASE)
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
+    // Start Server
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   })
-  .catch((error) => console.log(error));
-
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  });
